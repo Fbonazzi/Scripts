801db477ab23c46ae1beffea55b0f42ba2a230c9
Filippo Bonazzi <ippusonline@gmail.com>
2014-12-09 11:35:43 +0200

    Fixed primamaiuscola.sh parsing extension incorrectly in filenames with parenthesis. Added safe_regex.sh to fix that permanently

--- primamaiuscola.sh
+++ primamaiuscola.sh
@@ -10,8 +10,8 @@ do
 	#Nome del file
 	newname="${filename%.*}"
 	#Estensione, se presente
-	extn=$(echo "$filename" | sed -r "s/$newname(\..*|)$/\1/") # doppi apici necessari per sostituzione variabile
-
+	safename=$($HOME/Scaricati/Scripts/safe_regex.sh "$newname" extended)
+	extn=$(echo "$filename" | sed -r "s/${safename}(\..*|)$/\1/") # doppi apici necessari per sostituzione variabile
 
 	#rendi maiuscola la prima lettera di ogni parola del nome
 	newname=$(echo "$newname" | sed -r 's/\<([a-z])/\u\1/g')
@@ -20,5 +20,5 @@ do
 	newname=$(echo "$newname" | sed -r "s/'([A-Z])\>/'\L\1/g")
 
 	#finalmente sposta
-	mv "$name" "${newname}$extn"
+	echo "$name" "${newname}$extn"
 done
---
+++ safe_regex.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Escape sensitive characters in text strings to make them "literal" for sed LHS
+# Taken from http://backreference.org/2009/12/09/using-shell-variables-in-sed/
+
+extended=1
+
+# Change this, properly learn and implement getopts
+if [ $# -eq 1 ]
+then
+	pattern=$1
+elif [ $# -eq 2 ]
+then
+	extended=0	# True
+	if [ "$1" == "extended" ]
+	then
+		pattern=$2
+	elif [ "$2" == "extended" ]
+	then
+		pattern=$1
+	else
+		echo "Bad parameters: $1 $2"
+		exit -1
+	fi
+else
+	echo "Wrong number of parameters."
+	echo -e "\n Usage: $0 'regex_unsafe' [extended]"
+	exit -1
+fi
+
+if [ $extended ]
+then
+	safe_pattern=$(printf '%s\n' "$pattern" | sed 's/[[\.*^$(){}?+|/]/\\&/g')
+else
+	safe_pattern=$(printf '%s\n' "$pattern" | sed 's/[[\.*^$/]/\\&/g')
+fi
+printf '%s\n' "$safe_pattern"
+exit 0
