df3748f76956c7a6147b42e77f78a1e265c5248f
Filippo Bonazzi <ippusonline@gmail.com>
2016-09-14 11:00:01 +0300

    Support 0x-prefixed hexadecimal notation

--- binary/byte_to_bit.py
+++ binary/byte_to_bit.py
@@ -8,6 +8,8 @@ import sys
 import math
 
 s = "".join(sys.argv[1].split())
+if (s[0] == "0" and s[1] == "x"):
+    s = s[2:]
 for c in s:
     if c not in "0123456789abcdef":
         print("Bad string \"{}\"".format(s))
