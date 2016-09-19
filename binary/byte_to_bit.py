2e54d5d658ab0fd114059b7cddd64ab29db4c587
Filippo Bonazzi <ippusonline@gmail.com>
2016-09-19 16:51:42 +0300

    Handle uppercase hex notation, add comments

--- binary/byte_to_bit.py
+++ binary/byte_to_bit.py
@@ -5,13 +5,18 @@
 # representation using the "LSB 0" numbering scheme.
 # TODO: add argparse, support "MSB 0"
 import sys
-import math
 
+# Remove whitespace
 s = "".join(sys.argv[1].split())
+# Lowercase (support both upper and lower case hex notation)
+s = s.lower()
+# Strip leading "0x" if any
 if (s[0] == "0" and s[1] == "x"):
     s = s[2:]
+# Check string content
 for c in s:
     if c not in "0123456789abcdef":
         print("Bad string \"{}\"".format(s))
         sys.exit(1)
+# Convert
 print("{:#b}".format(int(s, 16)))
